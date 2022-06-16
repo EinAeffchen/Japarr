7eca813a0e10b84a2f03054bf399178fc99917f7
einaeffchen <github@dummer.dev>
2022-06-16 22:06:08 +0200

    Add basic construct

--- src/japarr/main.py
+++ src/japarr/main.py
@@ -10,18 +10,56 @@ from japarr.adapters.jraws import JRawsDownloader
 from japarr.media_objects import Movie, Drama
 
 class DownloadManager:
+    discord = DiscordConnector
+    sonarr = SonarrAdapter
+    radarr = RadarrAdapter
+    overseerr = OverseerAdapter
+    db = JaparrDB
+    jraw_adapter = JRawsDownloader
+    active = bool
+
     def __init__(self) -> None:
         self.discord = DiscordConnector()
         self.sonarr = SonarrAdapter(self.discord)
         self.radarr = RadarrAdapter(self.discord)
         self.overseerr = OverseerAdapter()
         self.db = JaparrDB()
-        self.jraw_adapter = JRawsDownloader
+        self.jraw_adapter = JRawsDownloader()
+        self.active = True
 
-    def start(self):
+    def start_movies(self):
+        movies = self.jraw_adapter.get_movies()
+        for movie in movies:
+            if self.active:
+                media_detail_dict = self.jraw_adapter.parse_media_details(movie.url)
+                movie.set_media_details(media_detail_dict)
+                #TODO download of files
+                # trigger to radarr/overseerr
+
+    def start_dramas(self):
+        """
+        Still has to be implemented
+        """
         pass
+            
+
+    def start_anime(self):
+        """
+        Still has to be implemented
+        """
+        pass
+    
+    def start(self):
+        self.active = True
+        self.start_movies()
+        self.start_dramas()
+        self.start_anime()
+
+    def stop(self):
+        self.active = False
 
 
 if __name__ == "__main__":
     dm = DownloadManager()
     dm.start()
+
